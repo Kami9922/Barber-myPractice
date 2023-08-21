bad03804f83057f85d2864189f777bab4497f3b7
Vitalya <vitalikt_22@mail.ru>
2023-08-21 07:40:58 +0400

    Third commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,11 @@
 // import mobileNav from './modules/mobile-nav.js'
+// mobileNav()
 import { init } from './modules/map.js'
 
-// mobileNav()
+const tabsBtn = document.querySelectorAll('.button-tab')
+
+tabsBtn.forEach((item) => {
+  item.addEventListener('click', function () {
+    console.log('click')
+  })
+})
